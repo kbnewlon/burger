d29fc6b5a184bda16f2918caddecedfd0872acb3
Kayla Newlon <kbnewlon@hotmail.com>
2020-10-16 23:20:50 -0700

    added burger buttons

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hickory Brisket Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Weggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
